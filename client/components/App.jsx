6fc0272a68a82d68ef6a356d1935c9207ec9c902
ddmcdona06 <darryldmcdonald@gmail.com>
2023-09-20 08:01:50 -0500

    controversy page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,6 +8,7 @@ import Navigation from "./Navigation.jsx";
 import axios from "axios";
 import Messages from "./MessageComponents/Messages.jsx";
 import SignUp from "./SignUp.jsx";
+import Controversy from "./Controversy.jsx";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -243,6 +244,11 @@ const App = () => {
           <Route
             path="/DecisionMaker"
             element={<DecisionMaker changePoints={changePoints} user={user} />}
+          />
+          <Route
+            exact
+            path="/Controversy"
+            element={<Controversy />}
           />
         </Route>
       </Routes>
---
+++ client/components/Controversy.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+const Controversy = () => {
+  return (
+    <div>
+      <h1>Hey Im rendering</h1>
+    </div>
+  );
+};
+
+export default Controversy;
