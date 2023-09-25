78d62cd7167acefb1a047a789bce9c6b04177a02
ddmcdona06 <darryldmcdonald@gmail.com>
2023-09-24 23:14:54 -0500

    added comment blocks

--- client/components/Controversy.jsx
+++ client/components/Controversy.jsx
@@ -42,7 +42,61 @@ const Controversy = () => {
               Grab A Headline
           </button>
         </div>
-        <textarea className='cont-box' placeholder='Speak on it...' />
+        <div className="container my-5 py-5 text-dark">
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-11 col-lg-9 col-xl-7">
+              <div className="d-flex flex-start mb-4">
+                <img className="rounded-circle shadow-1-strong me-3"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(32).webp" alt="avatar" width="65"
+                  height="65" />
+                <div className="card w-100">
+                  <div className="card-body p-4">
+                    <div className="">
+                      <h5>Logan</h5>
+                      <p className="small">3 hours ago</p>
+                      <p>
+                        Random text not really related to anything in particular, but I oppose what Nasthia said!
+                      </p>
+
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex align-items-center">
+                          <a href="#!" className="link-muted me-2"><i className="fas fa-thumbs-up me-1"></i>132</a>
+                          <a href="#!" className="link-muted"><i className="fas fa-thumbs-down me-1"></i>15</a>
+                        </div>
+                        <a href="#!" className="link-muted"><i className="fas fa-reply me-1"></i> Reply</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="d-flex flex-start">
+                <img className="rounded-circle shadow-1-strong me-3"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(31).webp" alt="avatar" width="65"
+                  height="65" />
+                <div className="card w-100">
+                  <div className="card-body p-4">
+                    <div className="">
+                      <h5>Nasthia</h5>
+                      <p className="small">5 hours ago</p>
+                      <p>
+                        Also, not responding to anything in particular, hard coded text!
+                      </p>
+
+                      <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex align-items-center">
+                          <a href="#!" className="link-muted me-2"><i className="fas fa-thumbs-up me-1"></i>158</a>
+                          <a href="#!" className="link-muted"><i className="fas fa-thumbs-down me-1"></i>13</a>
+                        </div>
+                        <a href="#!" className="link-muted"><i className="fas fa-reply me-1"></i> Reply</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
