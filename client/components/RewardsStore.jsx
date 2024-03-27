8ff038c189d57b7d7d5d3e547902847f5021b0bb
peytono <pcopcoeco@gmail.com>
2024-03-27 17:23:59 -0500

    RewardsStore has rewards state from rewards table on render

--- client/components/RewardsStore.jsx
+++ client/components/RewardsStore.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const RewardsStore = () => {
+  const [rewards, setRewards] = useState([]);
+
+  useEffect(() => {
+    axios.get('/rewards')
+      .then(({data}) => {
+        console.log(data);
+        setRewards(data);
+      })
+      .catch((err) => console.error('failed getting rewards'));
+  }, []);
+
   return (
     <div className="wof-component container">
       <h1>Rewards Store</h1>
